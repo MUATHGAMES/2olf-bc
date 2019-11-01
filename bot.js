8424b2e644194e0cac2c55eae778c2cf266dacd0
MUATHGAMES <56757519+MUATHGAMES@users.noreply.github.com>
2019-11-01 03:03:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("2oLF");
-var prefix = ".";
+const prefix = ".";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "la")) {
