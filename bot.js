5831cfebc2771358f9a11a9fd3ce70e67669a05b
MUATHGAMES <56757519+MUATHGAMES@users.noreply.github.com>
2019-11-02 00:53:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("2oLF");
 const prefix = "!";
-client.on("message", message => {
+/*client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
@@ -27,7 +27,71 @@ client.on("message", message => {
   message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
   message.delete();
   };
-  });
+  });*/
+
+const fs = require('fs');
+const ms = require("pretty-ms");
+
+const embedColor = "#36393e";
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers [ " ${client.guilds.size} " ] Users [ " ${client.users.size} " ] channels [ " ${client.channels.size} " ]`); 
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('Bot Is Online');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('BY 2oLF');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  client.user.setPresence({ game: { name: "$bc | $vip", type: "streaming", url: "https://www.twitch.tv/2oLF"}}); 
+});
+
+client.on('message', message => {
+if(!message.content.startsWith(prefix)) return;
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);if (command == "bc") {if(!message.member.roles.find('name','bc')) {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`**⛔ you must have \`ADMINISTRATOR\` premission**, or role called "bc"`)}
+let args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.channel.send(`**:rolling_eyes: please type the broadcast message**`)
+let filter = m => m.author.id == message.author.id
+let broadcastt = new Discord.RichEmbed().setColor('#36393e')
+.addField(`**[1] broadcast for all members\n\n[2] broadcast for online members\n\n[3] broadcast for a specific role\n\n[4] broadcast with photo\n\n[0] to cansel**`,`** **`)
+.setDescription(`**Please type the number of your chose**`)
+.setFooter('you can add to the message [user] = mention the user')
+message.channel.send(broadcastt).then(msg => {
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']})
+.then(collected => {if(collected.first().content === '1') {msg.delete(),message.channel.send(`**☑ Broadcast begin send....**`).then(m => {
+message.guild.members.map(member => {setTimeout(() => {member.send(args.replace('[user]',member).replace('[icon]',`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)).then(() => {}).catch((err) => {});},);});})}
+if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`**☑ Broadcast begin send....**`);
+message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {m.send(args.replace('[user]', m))})
+message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {m.send(args.replace('[user]', m)) })
+return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {m.send(args.replace('[user]', m)) })}
+if(collected.first().content === '0') {msg.delete(),message.channel.bulkDelete(1);return message.channel.send(`**Broadcast Has Been Canseled**`);}
+if(collected.first().content === '3') {msg.delete();message.channel.bulkDelete(1);
+message.channel.send('**Please Type the role name or id.**');
+message.channel.awaitMessages(filter, {max: 1,time: 40000,errors: ['time']}).then(t => {
+let R = t.first().content;
+let role = message.guild.roles.find('name',R) || message.guild.roles.get(R);
+if(!role) return message.channel.send('**😕 I Can\'t find this role please try again**'),msg.delete();
+message.channel.bulkDelete(2);
+if(role.members.size < 1) return message.channel.send('**there is no one have this role **😕');;
+let XYZ = new Discord.RichEmbed().setTitle('**:ballot_box_with_check: Broadcast begin send....**').setDescription(`**For the role: ${role}**`).setColor(role.color)
+message.channel.send(XYZ)
+message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {setTimeout(() => {n.send(args.replace('[user]',n)).catch((err) => {});});});}).catch(err =>{});}
+if(!collected.first().content.includes(['1','2','3','4','0'])) {msg.edit('Canceled.')}
+if(collected.first().content === '4') { msg.delete();
+message.channel.send('**✅ Please Type the photo link now**,Type "cansel" to cansel.').then(msgg =>{
+message.channel.awaitMessages(filter, {max: 1,time: 50000,errors: ['time']}).then(XX => {
+let photo = XX.first().content; if(photo == 'cansel') {message.channel.bulkDelete(2); return message.channel.send('**Broadcast Has Been Canseled**')}
+let embed = new Discord.RichEmbed().setImage(photo).setTitle(`**are you sure you want to send this? \`[y,n]\`**`).setColor('#36393e')
+message.channel.send(embed).catch(e =>{return message.channel.send('**The Photo link is wrong :x:**')});
+let filter = m => m.author.id == message.author.id
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']}).then(XD => {if(XD.first().content === 'y') {
+let bc = new Discord.RichEmbed().setTitle(`${args}`).setImage(photo).setFooter(message.guild.name,`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)
+message.channel.bulkDelete(2);msgg.delete();message.channel.send('**☑ Broadcast begin send....**');message.guild.members.map(member => {setTimeout(() => {member.send(bc)}
+)})}if(XD.first().content == 'n') {message.channel.bulkDelete(2);message.channel.send('**Broadcast Has Been Canseled**')}
+})}).catch(myst =>{msgg.edit('Timed out.');})})
+}if(collected.first().content === '5'){} // لو تبي تضيف شي خامس :]
+}).catch(mys =>{msg.edit('Timed out to chose.')})})}});
 
 const developers = ['602554672221257769','624551532431409152'];
  
